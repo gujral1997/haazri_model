513677dadd194908db845d8cb2c899ec592a4af5
Ansh Gujral <anshgujral1997@gmail.com>
2018-02-21 00:48:01 +0530

    rename folders and rename images scripts distinguished

---
+++ all_folders_rename.py
@@ -0,0 +1,22 @@
+import os
+
+cwd = os.getcwd()
+cwd += '/copied_dataset'
+FList = os.listdir(cwd)
+os.chdir(cwd)
+FListC = FList[0:]
+n = 0
+for j in range(0,len(FListC)):
+    new_cwd =cwd+'/'+str(n)
+    new_FList = os.listdir(new_cwd)
+    new_FListC = new_FList[0:]
+    os.chdir(new_cwd)
+    m = 0
+    for i in new_FListC:
+        fileExtension = os.path.splitext(i)[1]
+        os.rename(i,str(m)+fileExtension)
+        m = m+1
+    n=n+1
+    os.chdir('..')
+    os.chdir(new_cwd)
+#hello[0:len(hello)-2]
--- file_rename.py
+++ file_rename.py
@@ -1,23 +1,11 @@
+# renames all folders in copied_dataset to sequential whole numbers
 import os
 
-cwd = os.getcwd()
-cwd += '/copied_dataset'
-FList = os.listdir(cwd)
-os.chdir(cwd)
+FList = os.listdir(os.getcwd())
 FListC = FList[0:]
-n = 0
-for j in range(0,len(FListC)):
-    new_cwd =cwd+'/'+str(n)
-    new_FList = os.listdir(new_cwd)
-    new_FListC = new_FList[0:]
-    os.chdir(new_cwd)
-    m = 0
-    for i in new_FListC:
-        fileExtension = os.path.splitext(i)[1]
-        os.rename(i,str(m)+fileExtension)
-        m = m+1
-    n=n+1
-    l = len(str(m))+1
-    new_cwd = cwd[0:len(new_cwd)-l]
-    os.chdir(new_cwd)
-#hello[0:len(hello)-2]
+
+m = 0
+for i in FListC:
+    fileExtension = os.path.splitext(i)[1]
+    os.rename(i,str(m)+fileExtension)
+    m = m+1
